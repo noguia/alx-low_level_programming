378cc6144fe2d463c22d4e061e56344a887bc6ac
noguia <noguiaisback@gmail.com>
2023-04-27 09:57:41 -0700

    1. List length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * list_len - computes the number of nodes in a list
+ * @h: pointer to the head of the list
+ *
+ * Return: the number of nodes in the list
+ */
+
+/** by noguia */
+
+size_t list_len(const list_t *h)
+{
+	size_t count;
+
+	count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
