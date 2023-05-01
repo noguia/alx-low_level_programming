43554c62fbfffa3ca30880c39aa2e432124036c5
noguia <noguiaisback@gmail.com>
2023-05-01 05:26:11 -0700

    Pop

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/** by noguia */
+
+/**
+ * pop_listint - Removes the first node from a linked list
+ * and returns its integer value.
+ *
+ * @head: A pointer to the 1st node
+ *
+ * Return: The integer value of the first node on success, or 0 on failure.
+ */
+
+int		 pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *ptr;
+
+	if (!*head)
+		return (0);
+	ptr = *head;
+	n = ptr->n;
+	*head = ptr->next;
+	free(ptr);
+	return (n);
+}
