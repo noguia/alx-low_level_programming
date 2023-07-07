03a8336afb26580d829a7a4c24364eb64c0b8146
noguia <noguiaisback@gmail.com>
2023-07-07 07:53:24 -0700

    task 4 . 5 and 6 up to date

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/** by noguiaa */
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table
+ *
+ * Return: void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node, *next;
+
+	if (!ht)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			next = node->next;
+			free(node->key), free(node->value), free(node);
+			node = next;
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
