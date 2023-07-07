3df447a8d45663d5b9926c4dac8069fe89042384
noguia <noguiaisback@gmail.com>
2023-07-07 07:48:51 -0700

    task 1 . 2 and 3 up to date

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/** by noguiaa */
+
+/**
+ * key_index - map a key to an index
+ * @key: given key
+ * @size: size of hash table array
+ *
+ * Return: index
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
